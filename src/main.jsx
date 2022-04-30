aabfac574b43113c86e569f55bc9c19f0994067b
bourpie <pbcomweb@gmail.com>
2022-04-30 12:15:00 -0400

    1 file modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '/routes/home';
 import Produits from '/routes/products';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import './index.scss';
+
+const theme = createTheme();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <>
-      <BrowserRouter>
-        <nav>
-          <Link to="/">Accueil</Link>
-          <Link to="/produits">PProduits</Link>
-        </nav>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="relative" color="default" elevation={0}>
+          <Toolbar>
+            <CameraIcon sx={{ mr: 2 }} />
+            <Typography variant="h6" color="inherit" noWrap>
+              Album layout
+            </Typography>
+          </Toolbar>
+        </AppBar>
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -20,7 +44,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           </Routes>
         </main>
         <footer></footer>
-      </BrowserRouter>
-    </>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
