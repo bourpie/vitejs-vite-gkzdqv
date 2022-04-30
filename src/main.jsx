25ac8a39a758eab20b33472cb3ad7addfde0a8d2
bourpie <pbcomweb@gmail.com>
2022-04-30 11:42:10 -0400

    3 files modified

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,17 @@ import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <>
+    <nav></nav>
+    <main>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="produits" element={<Produits />} />
       </Routes>
     </BrowserRouter>
+    </main>
+    <footer></footer>
+    </>
   </React.StrictMode>
 );
