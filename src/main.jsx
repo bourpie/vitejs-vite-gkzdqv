4472176ab143132762dad04785359ed050b7c0e0
bourpie <pbcomweb@gmail.com>
2022-04-30 16:31:30 -0400

    1 file modified, 5 added, 5 deleted

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '/routes/home';
-import Produits from '/routes/products';
-import MyNavbar from '/Components/MyNavbar';
-import AppBar from '@mui/material/AppBar';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Home from './routes/home';
+import Produits from './routes/products';
+import MyNavbar from './Components/MyNavbar';
 import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import './index.scss';
 
 const theme = createTheme();
+const Store = createStore();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <MyNavbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="produits" element={<Produits />} />
-          </Routes>
-        </main>
-        <footer></footer>
-      </ThemeProvider>
-    </BrowserRouter>
+    <Provider store={Store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <MyNavbar />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="produits" element={<Produits />} />
+            </Routes>
+          </main>
+          <footer></footer>
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
