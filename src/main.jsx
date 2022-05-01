67883e19cc26f21a4fb4166f13c7e358d84f4227
bourpie <pbcomweb@gmail.com>
2022-05-01 09:50:07 -0400

    1 file modified, 7 added, 2 deleted

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './routes/home';
-import Produits from './routes/products';
+import Home from './Pages/Home';
+import Produits from './Pages/Products';
 import MyNavbar from './Components/MyNavbar';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -14,21 +14,19 @@ import './index.scss';
 const theme = createTheme();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <MyNavbar />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="produits" element={<Produits />} />
-            </Routes>
-          </main>
-          <footer></footer>
-        </ThemeProvider>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MyNavbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="produits" element={<Produits />} />
+          </Routes>
+        </main>
+        <footer></footer>
+      </ThemeProvider>
+    </BrowserRouter>
+  </Provider>
 );
