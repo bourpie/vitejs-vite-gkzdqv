fb64bdc973b65d5ee47a5d8f101741eeafd39a8b
bourpie <pbcomweb@gmail.com>
2022-05-01 09:53:57 -0400

    1 file modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import Produits from './Pages/Products';
+import Home from '../Pages/Home';
+import Produits from '../Pages/Products';
 import MyNavbar from './Components/MyNavbar';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
