7865f8fe2b22fd769ab0c97fb8555ac61074c0b0
bourpie <pbcomweb@gmail.com>
2022-05-01 08:10:54 -0400

    1 file modified, 2 added, 2 deleted

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,23 @@
-import { createStore, combineReducers } from 'redux';
-import CounterReducer from './CounterReducer';
-import CartReducer from './CartReducer';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import counterReducer from './counterReducer';
+import cartReducer from './cartReducer';
 
 const rootReducer = combineReducers({
-  CounterReducer,
-  CartReducer,
+  counterReducer,
+  cartReducer,
 });
 
-const store = createStore(rootReducer);
+// Curryfication
+// 0:50 https://www.udemy.com/course/react-formation-complete/learn/lecture/25742150#overview
+const customMiddleware = (store) => (next) => (action) => {
+  const actionModif = {
+    type: 'ADDCART',
+    payload: 234,
+  };
+
+  next(actionModif);
+};
+
+const store = createStore(rootReducer, applyMiddleware(customMiddleware));
 
 export default store;
