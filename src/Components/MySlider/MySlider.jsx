b7508b45e101a95d6eabe2234e5fcf0d1601ba49
bourpie <pbcomweb@gmail.com>
2022-05-02 08:46:02 -0400

    1 file modified

--- src/Components/MySlider/MySlider.jsx
+++ src/Components/MySlider/MySlider.jsx
@@ -18,7 +18,7 @@ function MySlider() {
               slideAnim.index === index + 1 ? 'slide active-anim' : 'slide'
             }
           >
-            {obj.title}
+            <img src={`/public/img${index + 1}.jpg`} alt="" />
           </div>
         );
       })}
